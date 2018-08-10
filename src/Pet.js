f0d1d063ad52c47b2e30e27d524873f76fe6e71c
Brian Holt <btholt@gmail.com>
2018-08-09 20:49:33 -0700

    3 - part one

---
+++ src/Pet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pet = props => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
+};
+
+export default Pet;
