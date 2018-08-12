4028c56f2f094f4a8cfdf0008ce3dd8588aa7dd9
Brian Holt <btholt@gmail.com>
2018-08-11 19:06:11 -0700

    3

--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pet;
