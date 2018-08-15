4af5c23e97ef15fb592cbf73043b56cadfb3a238
Brian Holt <btholt@gmail.com>
2018-08-14 21:18:23 -0700

    7

--- src/App.js
+++ src/App.js
@@ -1,22 +1,87 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
+import pf from "petfinder-client";
 import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import { Provider } from "./SearchContext";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: "Seattle, WA",
+      animal: "",
+      breed: "",
+      breeds: [],
+      handleAnimalChange: this.handleAnimalChange,
+      handleBreedChange: this.handleBreedChange,
+      handleLocationChange: this.handleLocationChange,
+      getBreeds: this.getBreeds
+    };
+  }
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+  handleAnimalChange = event => {
+    this.setState(
+      {
+        animal: event.target.value
+      },
+      this.getBreeds
+    );
+  };
+  handleBreedChange = event => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
+  getBreeds() {
+    if (this.state.animal) {
+      petfinder.breed
+        .list({ animal: this.state.animal })
+        .then(data => {
+          if (
+            data.petfinder &&
+            data.petfinder.breeds &&
+            Array.isArray(data.petfinder.breeds.breed)
+          ) {
+            this.setState({
+              breeds: data.petfinder.breeds.breed
+            });
+          } else {
+            this.setState({ breeds: [] });
+          }
+        })
+        .catch(console.error);
+    } else {
+      this.setState({
+        breeds: []
+      });
+    }
+  }
   render() {
     return (
       <div>
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
-        <Router>
-          <Results path="/" />
-          <Details path="/details/:id" />
-          <SearchParams path="/search-params" />
-        </Router>
+        <Provider value={this.state}>
+          <Router>
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
+          </Router>
+        </Provider>
       </div>
     );
   }
