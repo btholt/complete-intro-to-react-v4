b5894777147599bb4108801787ee7bb51cf98ff3
Brian Holt <btholt@gmail.com>
2018-08-14 21:18:23 -0700

    7

---
+++ src/SearchContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchContext = React.createContext({
+  location: "Seattle, WA",
+  animal: "",
+  breed: "",
+  breeds: [],
+  handleAnimalChange() {},
+  handleBreedChange() {},
+  handleLocationChange() {},
+  getBreeds() {}
+});
+
+export const Provider = SearchContext.Provider;
+export const Consumer = SearchContext.Consumer;
