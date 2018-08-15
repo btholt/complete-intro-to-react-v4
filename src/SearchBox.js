c0da3625726882dabc28255a3527f0170f31a9d7
Brian Holt <btholt@gmail.com>
2018-08-14 21:18:23 -0700

    7

---
+++ src/SearchBox.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { ANIMALS } from "petfinder-client";
+import { Consumer } from "./SearchContext";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <Consumer>
+        {context => (
+          <div className="search-params">
+            <form>
+              <label htmlFor="location">
+                Location
+                <input
+                  id="location"
+                  onChange={context.handleLocationChange}
+                  value={context.location}
+                  placeholder="Location"
+                />
+              </label>
+              <label htmlFor="animal">
+                Animal
+                <select
+                  id="animal"
+                  value={context.animal}
+                  onChange={context.handleAnimalChange}
+                  onBlur={context.handleAnimalChange}
+                >
+                  <option />
+                  {ANIMALS.map(animal => (
+                    <option key={animal} value={animal}>
+                      {animal}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label htmlFor="breed">
+                Breed
+                <select
+                  disabled={!context.breeds.length}
+                  id="breed"
+                  value={context.breed}
+                  onChange={context.handleBreedChange}
+                  onBlur={context.handleBreedChange}
+                >
+                  <option />
+                  {context.breeds.map(breed => (
+                    <option key={breed} value={breed}>
+                      {breed}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <button>Submit</button>
+            </form>
+          </div>
+        )}
+      </Consumer>
+    );
+  }
+}
+
+export default Search;
