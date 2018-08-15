32ec0a98ca1d3eef3e6fcb7ee83daab136a29fb2
Brian Holt <btholt@gmail.com>
2018-08-14 21:18:23 -0700

    7

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pf from "petfinder-client";
 import Pet from "./Pet";
+import SearchBox from "./SearchBox";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -37,6 +38,7 @@ class Results extends React.Component {
   render() {
     return (
       <div className="search">
+        <SearchBox />
         {this.state.pets.map(pet => {
           let breed;
           if (Array.isArray(pet.breeds.breed)) {
