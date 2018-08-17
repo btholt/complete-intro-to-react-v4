f7fd7f1db8408f71165ddcbdc10cc4c00572a859
Brian Holt <btholt@gmail.com>
2018-08-17 17:27:52 -0600

    8

--- src/App.js
+++ src/App.js
@@ -74,6 +74,11 @@ class App extends React.Component {
       <div>
         <header>
           <Link to="/">Adopt Me!</Link>
+          <Link to="/search-params">
+            <span aria-label="search" role="img">
+              🔍
+            </span>
+          </Link>
         </header>
         <Provider value={this.state}>
           <Router>
