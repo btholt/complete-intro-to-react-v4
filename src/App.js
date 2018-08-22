aa69e3ef0081d9f4572c20cca177139f9d9c87c2
Brian Holt <btholt@gmail.com>
2018-08-21 19:35:00 -0700

    add emotion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Router, Link } from "@reach/router";
 import pf from "petfinder-client";
 import Results from "./Results";
 import Details from "./Details";
+import NavBar from "./NavBar";
 import SearchParams from "./SearchParams";
 import { Provider } from "./SearchContext";
 
@@ -72,14 +73,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-          <Link to="/search-params">
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
+        <NavBar />
         <Provider value={this.state}>
           <Router>
             <Results path="/" />
---
+++ src/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "@reach/router";
+import styled, { keyframes } from "react-emotion";
+import colors from "./colors";
+
+const Container = styled("header")`
+  background-color: ${colors.dark};
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
+
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const SpyGlass = styled("span")`
+  display: inline-block;
+  animation: 1s ${Spin} linear infinite;
+`;
+
+const NavBar = () => (
+  <Container>
+    <NavLink to="/">Adopt Me!</NavLink>
+    <NavLink to="/search-params">
+      {/* eslint-disable-next-line */}
+      <SpyGlass aria-label="search" role="img">
+        🔍
+      </SpyGlass>
+    </NavLink>
+  </Container>
+);
+
+export default NavBar;
