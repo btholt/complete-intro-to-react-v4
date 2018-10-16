2cb431273ba2b14527711bd5e12e5d2dfa063ef8
droan <notdrone@users.noreply.github.com>
2018-10-17 02:23:13 +0530

    Fix typo
    thumnbail -> thumbnail

--- src/Carousel.js
+++ src/Carousel.js
@@ -38,7 +38,7 @@ class Carousel extends React.Component {
               key={photo.value}
               src={photo.value}
               className={index === active ? "active" : ""}
-              alt="animal thumnbail"
+              alt="animal thumbnail"
             />
           ))}
         </div>
